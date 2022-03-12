a8e0584cde53dad607ff0592ac2d9a04efb15dbb
AlexDoss42 <alex.doss42@gmail.com>
2022-03-11 19:00:59 -0700

    gets the nav bar into a bar and off the column layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
 
 function App() {
+  console.log(111, window.location.href)
   return (
     <div className="App">
       <nav>
-        <ul>
-            <li>Home</li>
-            <li>DNA</li>
-            <li>Tree Page</li>
-            <li>Search</li>
-            <li>Extras</li>
+        <h3  style={{ display: 'inline-block' }}>Ancestry</h3>
+        <ul style={{ listStyleType: "none", display: 'inline-block' }}>
+            <li style={{ display: 'inline-block' }}>Home</li>
+            <li style={{ display: 'inline-block' }}>DNA</li>
+            <li style={{ display: 'inline-block' }}>Tree Page</li>
+            <li style={{ display: 'inline-block' }}>Search</li>
+            <li style={{ display: 'inline-block' }}>Extras</li>
         </ul>
       </nav>
     </div>
