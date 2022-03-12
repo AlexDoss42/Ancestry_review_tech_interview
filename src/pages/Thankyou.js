74a590844695bf0bbb3583b3e6cc3c70f1603db7
AlexDoss42 <alex.doss42@gmail.com>
2022-03-11 21:08:24 -0700

    adds usenavigate to the thank you page

--- src/pages/Thankyou.js
+++ src/pages/Thankyou.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Thankyou = () => {
+    const navigate = useNavigate();
+
+
     return (
-        <h1>This is the Thank you page</h1>
+        <>
+            <h1>This is the Thank you page</h1>
+            <button>Leave Browser</button>
+            {/* WE are going to want to change that to the redux value for the page they hit the link for */}
+            {/* <button onClick={()=> navigate(window.location.href)}>Return to your page</button> */}
+        </>
     )
 };
 
