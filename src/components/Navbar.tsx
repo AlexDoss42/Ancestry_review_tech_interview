39f9014950e5e9fe3feab14d01cb48ac482baa54
AlexDoss42 <alex.doss42@gmail.com>
2022-04-13 19:46:08 -0600

    extracted styling for navbar to css file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = (): JSX.Element => {
 
     return (
-        <div style={{ background: "#38362D"}} >
+        <div className='container'>
             <nav>
-                <h3  style={{ display: 'inline-block', color: '#fff' }}>Ancestry</h3>
-                <ul style={{ listStyleType: "none", display: 'inline-block' }}>
-                    <li style={{ display: 'inline-block', marginRight: '10px' }}><Link to="/" style={{ color: '#fff' }}>Home</Link></li>
-                    <li style={{ display: 'inline-block', marginRight: '10px' }}><Link to="/dna" style={{ color: '#fff' }}>DNA</Link></li>
-                    <li style={{ display: 'inline-block', marginRight: '10px' }}><Link to="/tree" style={{ color: '#fff' }}>Tree Page</Link></li>
-                    <li style={{ display: 'inline-block', marginRight: '10px' }}><Link to="/search" style={{ color: '#fff' }}>Search</Link></li>
-                    <li style={{ display: 'inline-block', marginRight: '10px' }}><Link to="/extras" style={{ color: '#fff' }}>Extras</Link></li>
+                <h3>Ancestry</h3>
+                <ul>
+                    <li><Link className='link' to="/">Home</Link></li>
+                    <li><Link className='link' to="/dna">DNA</Link></li>
+                    <li><Link className='link' to="/tree">Tree Page</Link></li>
+                    <li><Link className='link' to="/search">Search</Link></li>
+                    <li><Link className='link' to="/extras">Extras</Link></li>
                 </ul>
             </nav>
         </div>
