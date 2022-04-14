eba3c9b326802eb4bc35cceb27057f2eb9a0f540
AlexDoss42 <alex.doss42@gmail.com>
2022-04-13 19:15:18 -0600

    updates the thank you page to typescript

--- src/pages/Thankyou.tsx
+++ src/pages/Thankyou.tsx
@@ -4,25 +4,25 @@ import { Link } from 'react-router-dom';
 
 
 const Thankyou = () => {
-    
-    const [path, setPath] = useState('');
+
+    const [path, setPath] = useState<string>('');
 
     useEffect(() => {
         setPath(JSON.parse(window.localStorage.getItem('path')));
       }, []);
 
     // it appears that you can only close a tab with scripts on windows you have opened. Stack overflow and a few sites suggested something like this but most likely wouldn't work across all broswers.  I would ask a senior/team for ideas but might push back on this one.
-    const onClose = async () => {
-        console.log('on Close');
-        window.opener = null;
-        await window.open("", "_self");
-        await window.close();
-      };
+    // const onClose = async () => {
+    //     console.log('on Close');
+    //     window.opener = null;
+    //     await window.open("", "_self");
+    //     await window.close();
+    //   };
 
     return (
         <>
             <h1>This is the Thank you page</h1>
-            <button onClick={() => onClose()}>Leave Browser</button>
+            {/* <button onClick={() => onClose()}>Leave Browser</button> */}
             <Link to={path}>Return to your page</Link>
         </>
     )
