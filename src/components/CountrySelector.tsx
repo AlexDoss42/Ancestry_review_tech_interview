2da65bb52e3bfcc0f500d083f977073953e4b2f1
AlexDoss42 <alex.doss42@gmail.com>
2022-04-14 22:00:07 -0600

    rangles in country selector style

--- src/components/CountrySelector.tsx
+++ src/components/CountrySelector.tsx
@@ -1,11 +1,13 @@
-import React, { useMemo } from 'react'
-import Select from 'react-select'
-import countryList from 'react-select-country-list'
+import React, { useMemo } from 'react';
+import Select from 'react-select';
+import countryList from 'react-select-country-list';
+
+import './CountrySelector.css';
 
 function CountrySelector({ countryHandler, displayCountry}): JSX.Element {
   const options = useMemo(() => countryList().getData(), [])
 
-  return <Select options={options} value={displayCountry} onChange={countryHandler} />
+  return <Select className='country-selector' options={options} value={displayCountry} onChange={countryHandler} />
 }
 
 export default CountrySelector;
