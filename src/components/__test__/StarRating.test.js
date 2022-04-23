8b8b2650638bec49cd322840715bc43dc2123c2b
AlexDoss42 <alex.doss42@gmail.com>
2022-04-22 19:05:41 -0600

    removes a console.log

--- src/components/__test__/StarRating.test.js
+++ src/components/__test__/StarRating.test.js
@@ -4,6 +4,5 @@ import StarRating from '../StarRating';
 it('should render 5 stars', () => {
   render(<StarRating experience_rating={0}/>);
   const starElements = screen.getAllByRole('radio');
-  console.log(123, starElements.length);
   expect(starElements.length).toBe(5);
 });
